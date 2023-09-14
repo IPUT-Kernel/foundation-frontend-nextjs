0afcf29e439e6ea530f695af76a5fb2a524b0ee1
cirrus-github <cirrus.joekuroha@gmail.com>
2023-09-14 18:11:49 +0900

    loginCallをtokenとuser分けるように変更

--- src/actionCalls.js
+++ src/actionCalls.js
@@ -1,10 +1,13 @@
 import axios from "axios";
 
-export const loginCall = async (user, dispatch) => {
+export const loginCall = async (userCreds, dispatch) => {
     dispatch({ type: "LOGIN_START" });
     try {
-        const res = await axios.post("api/auth/login", user);
-        dispatch({ type: "LOGIN_SUCCESS", payload: res.data });
+        const res = await axios.post("api/auth/login", userCreds);
+        dispatch({ 
+            type: "LOGIN_SUCCESS", 
+            payload: { user: res.data.user, token: res.data.token }
+        });
     } catch (err) {
         dispatch({ type: "LOGIN_ERROR", payload: err });
     }
