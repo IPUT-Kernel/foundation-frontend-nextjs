9576a2688b2cde9cb78dbb22ccb9f03bf07e7e5e
cirrus-github <cirrus.joekuroha@gmail.com>
2023-09-14 18:30:07 +0900

    DBG

--- src/actionCalls.js
+++ src/actionCalls.js
@@ -1,9 +1,11 @@
 import axios from "axios";
 
 export const loginCall = async (userCreds, dispatch) => {
+    console.log("loginCall function called");  // このログを追加
     dispatch({ type: "LOGIN_START" });
     try {
         const res = await axios.post("api/auth/login", userCreds);
+        console.log("Response from server:", res.data);  // このログを追加
         console.log("Token from response:", res.data.token);  // ここでログを出力
         dispatch({ 
             type: "LOGIN_SUCCESS", 
