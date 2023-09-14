8eea0dfbd1a68a25ea9dc19e863ad0c9e6b7928b
cirrus-github <cirrus.joekuroha@gmail.com>
2023-09-14 18:18:16 +0900

    test

--- src/actionCalls.js
+++ src/actionCalls.js
@@ -4,6 +4,7 @@ export const loginCall = async (userCreds, dispatch) => {
     dispatch({ type: "LOGIN_START" });
     try {
         const res = await axios.post("api/auth/login", userCreds);
+        console.log("Token from response:", res.data.token);  // ここでログを出力
         dispatch({ 
             type: "LOGIN_SUCCESS", 
             payload: { user: res.data.user, token: res.data.token }
