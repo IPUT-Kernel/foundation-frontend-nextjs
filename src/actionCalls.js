74d909e35801996534f7203befbb82e72d69c6d2
cirrus-github <cirrus.joekuroha@gmail.com>
2023-09-15 20:54:21 +0900

    bug-fix

--- src/actionCalls.js
+++ src/actionCalls.js
@@ -4,7 +4,7 @@ export const loginCall = async (userCreds, dispatch) => {
     console.log("loginCall function called");  // このログを追加
     dispatch({ type: "LOGIN_START" });
     try {
-        const res = await axios.post("api/auth/login", userCreds);
+        const res = await axios.post("/api/auth/login", userCreds);
         console.log("Response from server:", res.data);  // このログを追加
         console.log("Token from response:", res.data.token);  // ここでログを出力
         dispatch({ 
