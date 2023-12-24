bfd88f0a2046deee73a623a12a781db0c57e0242
bitcat <kurohaj96@gmail.com>
2023-12-25 02:23:04 +0900

    パス修正

--- src/actionCalls.js
+++ src/actionCalls.js
@@ -3,9 +3,9 @@ import axios from "axios";
 export const loginCall = async (userCreds, dispatch) => {
     dispatch({ type: "LOGIN_START" });
     try {
-        const res = await axios.post("/api/auth/login", userCreds);
-        console.log("Response from server:", res.data);  // このログを追加
-        console.log("Token from response:", res.data.token);  // ここでログを出力
+        const res = await axios.post("/v1/auth/login", userCreds);
+        console.log("Response from server:", res.data);
+        console.log("Token from response:", res.data.token); 
         dispatch({ 
             type: "LOGIN_SUCCESS", 
             payload: { user: res.data.user, token: res.data.token }
